043e8272d7875a4bb53ff09a39fb734fe4c94a34
Chen Wu <201436728@qq.com>
2021-02-11 19:26:33 +0800

    Readd android application

---
+++ app/src/main/java/io/github/qingchenw/microcontroller/ui/TestFragment.java
@@ -0,0 +1,12 @@
+package io.github.qingchenw.microcontroller.ui;
+
+import androidx.fragment.app.Fragment;
+
+/*
+* 用于调试串口,蓝牙,wifi
+* 包含选择设备, 设置参数, 连接/断开连接, 日志框, 输入框(可能还需要设置以16进制还是文本形式发送), 发送按钮
+*/
+public class TestFragment extends Fragment {
+
+
+}
